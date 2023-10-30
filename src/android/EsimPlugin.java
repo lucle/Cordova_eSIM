e13717f581fedd48476fc3b59ba187a6df8af77b
Lux Le <luclt399471@gmail.com>
2023-10-30 15:17:28 +0700

    Update EsimPlugin.java

--- src/android/EsimPlugin.java
+++ src/android/EsimPlugin.java
@@ -1,41 +1,104 @@
 package com.dreamcloud;
-
 // The native android API
 import android.telephony.euicc.EuiccManager;
-import android.content.Context;
+import android.telephony.euicc.DownloadableSubscription;
+import android.telephony.TelephonyManager;
 
+import android.content.Context;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.app.PendingIntent;
 // Cordova-required packages
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.PluginResult;
 import org.apache.cordova.PluginResult.Status;
+import org.apache.cordova.LOG;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class EsimPlugin extends CordovaPlugin {
+public class EsimPlugin extends CordovaPlugin{
+    protected static final String LOG_TAG = "eSIM";
+    private static final String HAS_ESIM_ENABLED = "hasEsimEnabled";
+    private static final String INSTALL_ESIM = "installEsim";
+    private static final String ACTION_DOWNLOAD_SUBSCRIPTION = "download_subscription";
+    private Context mainContext;
+    private EuiccManager mgr;
+    
+    private static final String LPA_DECLARED_PERMISSION = "com.starhub.aduat.torpedo.lpa.permission.BROADCAST";
+    String address;
+    String matchingID;
+    String activationCode ;
 
-  private static final String HAS_ESIM_ENABLED = "hasEsimEnabled";
-  private CallbackContext callback;
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        mainContext = this.cordova.getActivity().getApplicationContext();
+     }
 
-  @Override
-  public boolean execute(String action, JSONArray args,
-    final CallbackContext callbackContext) throws JSONException {
-
-    callback = callbackContext;
-
-    if (HAS_ESIM_ENABLED.equals(action)) {
-      hasEsimEnabled();
-      return true;
-    } else {
-      return false;
+    @Override
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        if (HAS_ESIM_ENABLED.equals(action)) {
+            hasEsimEnabled(callbackContext);
+        }else if (INSTALL_ESIM.equals(action)) {   
+            installEsim(args, callbackContext);
+        }else {
+            return false;
+        }    
+        return true;
     }
-  }
 
-  private void hasEsimEnabled() {
-      Context context = this.cordova.getActivity().getApplicationContext();
-      EuiccManager mgr = (EuiccManager) context.getSystemService(Context.EUICC_SERVICE);
-      boolean result = mgr.isEnabled();
-      callback.sendPluginResult(new PluginResult(Status.OK, result));
-  }
+    private void initMgr() {
+        if (mgr == null) {
+          mgr = (EuiccManager) mainContext.getSystemService(Context.EUICC_SERVICE);
+        }
+    }
+
+    private void hasEsimEnabled(CallbackContext callbackContext) {
+        initMgr();
+        boolean result = mgr.isEnabled();
+        callbackContext.sendPluginResult(new PluginResult(Status.OK, result));
+    }
+
+    private void installEsim(JSONArray args, CallbackContext callbackContext) {         
+        try{
+            initMgr();             
+            BroadcastReceiver receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (!action.equals(intent.getAction())) {
+                    return;
+                }
+                int resultCode = getResultCode();
+                int detailedCode = intent.getIntExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_DETAILED_CODE, 0 /* defaultValue*/);
+            }
+        };
+        mainContext.registerReceiver(
+                receiver,
+                new IntentFilter(ACTION_DOWNLOAD_SUBSCRIPTION),
+                LPA_DECLARED_PERMISSION /* broadcastPermission*/, 
+                null /* handler */);
+
+        address = args.getString(0);
+        matchingID = args.getString(1);
+        activationCode = "1$" + address + "$" + matchingID;
+        // Download subscription asynchronously.
+        DownloadableSubscription sub =
+                DownloadableSubscription.forActivationCode(activationCode /* encodedActivationCode*/);
+        Intent intent = new Intent(action);
+        PendingIntent callbackIntent = PendingIntent.getBroadcast(
+            getContext(), 0 /* requestCode */, 
+            intent, 
+            PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
+        mgr.downloadSubscription(sub, true /* switchAfterDownload */, callbackIntent);
+        //callbackContext.sendPluginResult(new PluginResult(Status.OK, true));
+        }catch (Exception e) {
+            callbackContext.error("Error install eSIM "  + e.getMessage());
+            callbackContext.sendPluginResult(new PluginResult(Status.ERROR));
+        }
+    }       
 }
